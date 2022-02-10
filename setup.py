3d69298867fb0f02aef20e185b4e976afe0c0d1b
Jeremy Wright <wrightjmf@gmail.com>
2022-02-10 06:42:08 -0500

    Trying to get a platform wheel built

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name="ocp",
     version="7.5.3",
     packages=find_packages(include=["OCP", "OCP.*"]),
+    package_data={'OCP': ['OCP.cpython-39-x86_64-linux-gnu.so']},
     install_requires=[
         'boost>=1.74.0',
         'joblib',
