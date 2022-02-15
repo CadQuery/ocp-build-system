b4051a6cc9d4bda706faa3978db1de354987bba9
Jeremy Wright <wrightjmf@gmail.com>
2022-02-15 15:17:07 -0500

    Syncing before trying auditwheel

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ setup(
     name="ocp",
     version="7.5.3",
     packages=find_packages(include=["OCP", "OCP.*"]),
-    package_data={'OCP': ['OCP.cpython-39-x86_64-linux-gnu.so']},
     install_requires=[
         'boost>=1.74.0',
         'joblib',
