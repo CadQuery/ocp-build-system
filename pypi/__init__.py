e786168c97f1508b9bd7e76d5796ba98d4b471e7
Bernhard <b_walter@arcor.de>
2025-01-17 17:07:20 +0100

    fix version

--- pypi/__init__.py
+++ pypi/__init__.py
@@ -1,2 +1,5 @@
 from OCP.OCP import *
-from OCP.OCP import __version__
+
+# from OCP.OCP import __version__
+
+__version__ = "7.8.1.1"
--- pypi/__init__win.py
+++ pypi/__init__win.py
@@ -17,4 +17,6 @@ _vtkmodules()
 del _vtkmodules
 
 from OCP.OCP import *
-from OCP.OCP import __version__
+
+# from OCP.OCP import __version__
+__version__ = "7.8.1.1"
