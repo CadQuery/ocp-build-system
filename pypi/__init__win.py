dc080de0e9b2f53765307048f00cedfdd4a3de96
Bernhard <b_walter@arcor.de>
2025-09-20 14:20:28 +0200

    remove special Py 3.13 handling

--- pypi/__init__win.py
+++ pypi/__init__win.py
@@ -1,15 +1,19 @@
 def _vtkmodules():
     import os
-    import sys
+    # import sys
 
-    if sys.version_info[0] == 3 and sys.version_info[1] == 13:
-        libs_dir = os.path.abspath(
-            os.path.join(os.path.dirname(__file__), os.pardir, "cadquery_vtk.libs")
-        )
-    else:
-        libs_dir = os.path.abspath(
-            os.path.join(os.path.dirname(__file__), os.pardir, "vtk.libs")
-        )
+    # if sys.version_info[0] == 3 and sys.version_info[1] == 13:
+    #     libs_dir = os.path.abspath(
+    #         os.path.join(os.path.dirname(__file__), os.pardir, "cadquery_vtk.libs")
+    #     )
+    # else:
+    #     libs_dir = os.path.abspath(
+    #         os.path.join(os.path.dirname(__file__), os.pardir, "vtk.libs")
+    #     )
+
+    libs_dir = os.path.abspath(
+        os.path.join(os.path.dirname(__file__), os.pardir, "vtk.libs")
+    )
     os.add_dll_directory(libs_dir)
 
 
